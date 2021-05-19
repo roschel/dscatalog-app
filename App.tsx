4dd4bb7cb2fbc6c5b1034d963b1595da56226dae
João Roschel <joaomroschel@gmail.com>
2021-05-18 21:16:32 -0300

    few changes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
 
-export default function App() {
-  return (
+/**
+ * flex-direction: row - justify-content alinha horizontalmente
+ * quando column (default) - justify content alinha verticalmente
+ * @returns 
+ */
+const App = () =>{
+  return(
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.h1}>Hi, there</Text>
+      <Text style={styles.h2}>Dev: João</Text>
+      <Text style={styles.h3}>Aluno</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+      backgroundColor: '#069',
+      flex:1,
+      alignItems:'center', // horizontal
+      justifyContent:'center' // vertical
   },
-});
+  h1:{
+    fontSize:24,
+    fontWeight:'700',
+    color:'#fff'
+  },
+  h2:{
+    fontSize:18,
+    fontWeight:'500',
+    color:'#fff'
+  },
+  h3:{
+    fontSize:14,
+    color:'#ccc'
+  },
+})
+
+export default App;
